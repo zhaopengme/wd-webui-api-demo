a2e55e560add699ab7472a1df22fbdfd2ce81817
zhaopengme <mikey.zhao@gitnoteapp.com>
2023-07-02 14:21:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+### Backend setup
+
+Follow the instuction of https://github.com/nftblackmagic/sdwebui-api-manager
+
+### Enviroment var
+
+Create a .env file, add NEXT_PUBLIC_API_URL as env var
+
+```
+NEXT_PUBLIC_API_URL=http://0.0.0.0
+NEXT_PUBLIC_API_PORT=5001
+```
+
+### Install
+
+```
+yarn install
+```
+
+### Run website
+
+```
+yarn run dev
+```
+
+Open http://localhost:3000 with your browser to see the result.
+
+You can start editing the page by modifying app/page.tsx. The page auto-updates as you edit the file.
---
+++ app/(generatePage)/txt2img/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import { ImageGallery } from "../../component/ImageGallery";
+
+import * as _ from "lodash";
+import { ControlNetInput } from "@/app/component/ControlNetInput";
+import { CommonInput } from "@/app/component/CommonInput";
+import { PromptContainer } from "@/app/component/PromptContainer";
+import { ImageGenerationButton } from "@/app/component/ImageGenerationButton";
+
+export default function RealTxtPage() {
+  const [result, setResult] = useState<any>(null);
+
+  const setFinalResults = (result: any) => {
+    setResult(result);
+  };
+
+  return (
+    <div className="p-10 flex flex-col gap-2 ">
+      <h1>A general text to image generator</h1>
+      <PromptContainer mode={0} display={true} />
+      <CommonInput mode={0} />
+      <ControlNetInput mode={0} />
+      <ImageGenerationButton
+        setFinalResults={setFinalResults}
+        defaultCheckpoint={"sd-v1-5-inpainting.ckpt [c6bbc15e32]"}
+        mode={0}
+      />
+      {result && <ImageGallery result={result} />}
+    </div>
+  );
+}
